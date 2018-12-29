731f8c43ed337c240ad852da1b9e04c764a2299f
Yasin Malli <yasinmalli@gmail.com>
2018-12-28 20:24:34 -0800

    adding appsettings.json to Migrations project.

--- yapa-migrations/Program.cs
+++ yapa-migrations/Program.cs
@@ -1,6 +1,8 @@
 ﻿using FluentMigrator.Runner;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System;
+using System.IO;
 using yapa_migrations.Migrations;
 
 namespace yapa_migrations
@@ -19,11 +21,13 @@ namespace yapa_migrations
 
         private static IServiceProvider CreateServices()
         {
+            var connectionString = GetConnectionString();
+
             return new ServiceCollection()
                 .AddFluentMigratorCore()
                 .ConfigureRunner(rb => rb                    
                     .AddPostgres()
-                    .WithGlobalConnectionString("Server=localhost;Port=5432;User Id=postgres;Password=Password1;Database=users;")
+                    .WithGlobalConnectionString(connectionString)
                     .ScanIn(typeof(M10_CreateExpenses).Assembly).For.Migrations())
                 .AddLogging(lb => lb.AddFluentMigratorConsole())
                 .BuildServiceProvider(false);
@@ -34,6 +38,19 @@ namespace yapa_migrations
             var runner = serviceProvider.GetRequiredService<IMigrationRunner>();
 
             runner.MigrateUp();
+        }
+
+        private static string GetConnectionString()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+            IConfigurationRoot configuration = builder.Build();
+            
+            return string.Format("Server={0};Port={1};User Id={2};Password={3};Database={4};",
+                configuration["db_server"], configuration["db_port"], configuration["db_user_id"],
+                configuration["db_password"], configuration["db_name"]);            
         }
     }
 }
